1644edd72faf8ee8f69e1011b9998756816e59d0
Ivan Wang <33285345+iwang1959@users.noreply.github.com>
2024-11-09 18:22:36 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,16 @@ import streamlit as st
 
 st.title('Stock Price LSTM')
 
-st.write('This application uses the Long short-term memory (LSTM) to determine stock price.
+st.write('This application uses the Long short-term memory (LSTM) to determine stock price.')
 
-RECURRENT NEURAL NETWORK
+st.write('RECURRENT NEURAL NETWORK')
 
-It is one of the oldest networks which was created in the year the 1980s, which has recently became popular due to highly generated data and high computation power of computers.
+st.write('It is one of the oldest networks which was created in the year the 1980s, which has recently became popular due to highly generated data and high computation power of computers.')
 
-The main uses of RNN is for sequence and time series data
+st.write('The main uses of RNN is for sequence and time series data')
 
-RNN is one of the most powerful algorithms where it has internal memory to store previous data. One of the main features of RNN is that the internal memory helps to remeber important things and this allows the computer to make predictions on what comes next.
+st.write('RNN is one of the most powerful algorithms where it has internal memory to store previous data. One of the main features of RNN is that the internal memory helps to remeber important things and this allows the computer to make predictions on what comes next.')
 
-RNN is a feedback neutral network where it has self-loops at the hidden layer.
+st.write('RNN is a feedback neutral network where it has self-loops at the hidden layer.')
 
-One drawback of this algorithm is that RNN has short-term memory which stores only short span of information.')
+st.write('One drawback of this algorithm is that RNN has short-term memory which stores only short span of information.')
